113a47644d1f4cd440f2b55aee85b3f33402bf49
RAHAT-RH <alaminrahat12@gmail.com>
2023-01-25 03:18:10 +0600

    link updated

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -4,60 +4,60 @@ import { Link } from 'react-router-dom';
 const Checkout = () => {
     return (
         <div>
-            <div class="container grid lg:grid-cols-1 sm:grid-cols-1 items-start pb-16 pt-4 gap-6">
+            <div className="container grid lg:grid-cols-1 sm:grid-cols-1 items-start pb-16 pt-4 gap-6">
 
-                <div class="col-span-8 border bg-white border-gray-200 p-4 rounded">
-                    <h3 class="text-lg font-medium capitalize mb-4 text-black">Shopping Details</h3>
-                    <div class="space-y-4">
-                        {/* <div class="grid  grid-cols-1 gap-4"> */}
+                <div className="col-span-8 border bg-white border-gray-200 p-4 rounded">
+                    <h3 className="text-lg font-medium capitalize mb-4 text-black">Shopping Details</h3>
+                    <div className="space-y-4">
+                        {/* <div className="grid  grid-cols-1 gap-4"> */}
 
-                            <div class="p-6 w-full">
+                            <div className="p-6 w-full">
 
-                                <div class="grid grid-cols-2 gap-4">
-                                    <div class="form-group mb-6">
+                                <div className="grid grid-cols-2 gap-4">
+                                    <div className="form-group mb-6">
                                         <label for="email" className="text-[#0364BE] mb-2 block">Email address</label>
                                         <input type="email" name="email" id="email"
                                             className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                                             placeholder="youremail.@gmail.com" />
                                     </div>
-                                    <div class="form-group mb-6">
+                                    <div className="form-group mb-6">
                                         <label for="email" className="text-[#0364BE] mb-2 block">Email address</label>
                                         <input type="email" name="email" id="email"
                                             className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                                             placeholder="youremail.@gmail.com" />
                                     </div>
                                 </div>
-                                <div class="form-group mb-6">
+                                <div className="form-group mb-6">
 
                                     <input type="email" name="email" id="email"
                                         className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                                         placeholder="youremail.@gmail.com" />
                                 </div>
-                                <div class="form-group mb-6">
+                                <div className="form-group mb-6">
 
                                     <input type="email" name="email" id="email"
                                         className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                                         placeholder="youremail.@gmail.com" />
                                 </div>
 
-                                <div class="flex justify-between py-3">
-                                    <h4 class="text-black">Billing Information</h4>
-                                    <span class='text-primary '><input type="checkbox" class="bg-white outline-none" name="same" value='same' id="" /> Same as shipping details</span>
+                                <div className="flex justify-between py-3">
+                                    <h4 className="text-black">Billing Information</h4>
+                                    <span className='text-primary '><input type="checkbox" className="bg-white outline-none" name="same" value='same' id="" /> Same as shipping details</span>
                                 </div>
 
-                                <div class="form-group mb-6">
+                                <div className="form-group mb-6">
+
+                                    <input type="email" name="email" id="email"
+                                        classNameName="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
+                                        placeholder="youremail.@gmail.com" />
+                                </div>
+                                <div className="form-group mb-6">
 
                                     <input type="email" name="email" id="email"
                                         className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                                         placeholder="youremail.@gmail.com" />
                                 </div>
-                                <div class="form-group mb-6">
-
-                                    <input type="email" name="email" id="email"
-                                        className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
-                                        placeholder="youremail.@gmail.com" />
-                                </div>
-                                <div class="form-group mb-6">
+                                <div className="form-group mb-6">
 
                                     <input type="email" name="email" id="email"
                                         className="block w-full border bg-white border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
@@ -71,74 +71,74 @@ const Checkout = () => {
                     </div>
                 </div>
 
-                <div class="col-span-4 border bg-white w-full border-gray-200 p-4 rounded">
-                    <h4 class="text-gray-800 text-lg mb-4 font-medium uppercase">order summary</h4>
-                    <div class="space-y-2">
-                        <div class="flex justify-between">
+                <div className="col-span-4 border bg-white w-full border-gray-200 p-4 rounded">
+                    <h4 className="text-gray-800 text-lg mb-4 font-medium uppercase">order summary</h4>
+                    <div className="space-y-2">
+                        <div className="flex justify-between">
                             <div>
-                                <h5 class="text-gray-800 font-medium">T-Shirt</h5>
-                                <p class="text-sm text-gray-600">Size: M</p>
+                                <h5 className="text-gray-800 font-medium">T-Shirt</h5>
+                                <p className="text-sm text-gray-600">Size: M</p>
                             </div>
-                            <p class="text-gray-600">
+                            <p className="text-gray-600">
                                 x3
                             </p>
-                            <p class="text-gray-800 font-medium">$320</p>
+                            <p className="text-gray-800 font-medium">$320</p>
                         </div>
-                        <div class="flex justify-between">
+                        <div className="flex justify-between">
                             <div>
-                                <h5 class="text-gray-800 font-medium">Huddy</h5>
-                                <p class="text-sm text-gray-600">Size: M</p>
+                                <h5 className="text-gray-800 font-medium">Huddy</h5>
+                                <p className="text-sm text-gray-600">Size: M</p>
                             </div>
-                            <p class="text-gray-600">
+                            <p className="text-gray-600">
                                 x3
                             </p>
-                            <p class="text-gray-800 font-medium">$320</p>
+                            <p className="text-gray-800 font-medium">$320</p>
                         </div>
-                        <div class="flex justify-between">
+                        <div className="flex justify-between">
                             <div>
-                                <h5 class="text-gray-800 font-medium">Casual Shirt</h5>
-                                <p class="text-sm text-gray-600">Size: M</p>
+                                <h5 className="text-gray-800 font-medium">Casual Shirt</h5>
+                                <p className="text-sm text-gray-600">Size: M</p>
                             </div>
-                            <p class="text-gray-600">
+                            <p className="text-gray-600">
                                 x3
                             </p>
-                            <p class="text-gray-800 font-medium">$320</p>
+                            <p className="text-gray-800 font-medium">$320</p>
                         </div>
-                        <div class="flex justify-between">
+                        <div className="flex justify-between">
                             <div>
-                                <h5 class="text-gray-800 font-medium">Boxer</h5>
-                                <p class="text-sm text-gray-600">Size: M</p>
+                                <h5 className="text-gray-800 font-medium">Boxer</h5>
+                                <p className="text-sm text-gray-600">Size: M</p>
                             </div>
-                            <p class="text-gray-600">
+                            <p className="text-gray-600">
                                 x3
                             </p>
-                            <p class="text-gray-800 font-medium">$320</p>
+                            <p className="text-gray-800 font-medium">$320</p>
                         </div>
                     </div>
 
-                    <div class="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercase">
+                    <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercase">
                         <p>subtotal</p>
                         <p>$1280</p>
                     </div>
 
-                    <div class="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercase">
+                    <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercase">
                         <p>shipping</p>
                         <p>Free</p>
                     </div>
 
-                    <div class="flex justify-between text-gray-800 font-medium py-3 uppercase">
-                        <p class="font-semibold">Total</p>
+                    <div className="flex justify-between text-gray-800 font-medium py-3 uppercase">
+                        <p className="font-semibold">Total</p>
                         <p>$1280</p>
                     </div>
 
-                    <div class="flex items-center mb-4 mt-2">
+                    <div className="flex items-center mb-4 mt-2">
                         <input type="checkbox"
-                            class="text-primary focus:ring-0 rounded-sm cursor-pointer w-3 h-3" />
-                        <label class="text-gray-600 ml-3 cursor-pointer text-sm">I agree to the <Link to="/" class="text-primary">terms & conditions</Link></label>
+                            className="text-primary focus:ring-0 rounded-sm cursor-pointer w-3 h-3" />
+                        <label className="text-gray-600 ml-3 cursor-pointer text-sm">I agree to the <Link to="/" className="text-primary">terms & conditions</Link></label>
                     </div>
 
                     <Link to=""
-                        class="block w-full py-3 px-4 text-center text-white bg-gradient-to-l from-primary to-[#52a3eb] hover:from-[#52a3eb] hover:to-primary transition font-medium">Place
+                        className="block w-full py-3 px-4 text-center text-white bg-gradient-to-l from-primary to-[#52a3eb] hover:from-[#52a3eb] hover:to-primary transition font-medium">Place
                         order</Link>
                 </div>
 
--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -7,124 +7,124 @@ import { RiFacebookFill, RiInstagramLine, RiTwitterLine, RiShoppingBagFill   } f
 const SingleProduct = () => {
     return (
         <div>
-            <div class="container my-16 grid lg:grid-cols-2 sm:grid-cols-1 py-16 bg-white gap-6">
-                <div class="w-full bg-cover bg-no-repeat bg-center">
-                    <img src={product1} alt="product" class="item-thumbnail w-full h-full object-contain" />
+            <div className="container my-16 grid lg:grid-cols-2 sm:grid-cols-1 py-16 bg-white gap-6">
+                <div className="w-full bg-cover bg-no-repeat bg-center">
+                    <img src={product1} alt="product" className="item-thumbnail w-full h-full object-contain" />
                 </div>
 
                 <div>
-                    <h2 class="text-3xl font-medium uppercase mb-2">Italian L Shape Sofa</h2>
-                    <div class="flex items-center mb-4">
-                        <div class="flex gap-1 text-sm text-yellow-400">
+                    <h2 className="text-3xl font-medium uppercase mb-2">Italian L Shape Sofa</h2>
+                    <div className="flex items-center mb-4">
+                        <div className="flex gap-1 text-sm text-yellow-400">
                             Best Product
                         </div>
-                        <div class="text-xs text-gray-500 ml-3">(150 Reviews)</div>
+                        <div className="text-xs text-gray-500 ml-3">(150 Reviews)</div>
                     </div>
-                    <div class="space-y-2">
-                        <p class="text-gray-800 font-semibold space-x-2">
+                    <div className="space-y-2">
+                        <p className="text-gray-800 font-semibold space-x-2">
                             <span>Availability: </span>
-                            <span class="text-green-600">In Stock</span>
+                            <span className="text-green-600">In Stock</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">Brand: </span>
-                            <span class="text-gray-600">Apex</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">Brand: </span>
+                            <span className="text-gray-600">Apex</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">Category: </span>
-                            <span class="text-gray-600">Sofa</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">Category: </span>
+                            <span className="text-gray-600">Sofa</span>
                         </p>
-                        <p class="space-x-2">
-                            <span class="text-gray-800 font-semibold">SKU: </span>
-                            <span class="text-gray-600">BE45VGRT</span>
+                        <p className="space-x-2">
+                            <span className="text-gray-800 font-semibold">SKU: </span>
+                            <span className="text-gray-600">BE45VGRT</span>
                         </p>
                     </div>
-                    <div class="flex items-baseline mb-1 space-x-2 font-roboto mt-4">
-                        <p class="text-xl text-primary font-semibold">$45.00</p>
-                        <p class="text-base text-gray-400 line-through">$55.00</p>
+                    <div className="flex items-baseline mb-1 space-x-2 font-roboto mt-4">
+                        <p className="text-xl text-primary font-semibold">$45.00</p>
+                        <p className="text-base text-gray-400 line-through">$55.00</p>
                     </div>
 
-                    <p class="mt-4 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eius eum
+                    <p className="mt-4 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eius eum
                         reprehenderit dolore vel mollitia optio consequatur hic asperiores inventore suscipit, velit
                         consequuntur, voluptate doloremque iure necessitatibus adipisci magnam porro.</p>
 
-                    <div class="pt-4">
-                        <h3 class="text-sm text-gray-800 uppercase mb-1">Size</h3>
-                        <div class="flex items-center gap-2">
-                            <div class="size-selector">
-                                <input type="radio" name="size" id="size-xs" class="hidden" />
+                    <div className="pt-4">
+                        <h3 className="text-sm text-gray-800 uppercase mb-1">Size</h3>
+                        <div className="flex items-center gap-2">
+                            <div className="size-selector">
+                                <input type="radio" name="size" id="size-xs" className="hidden" />
                                 <label for="size-xs"
-                                    class="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">XS</label>
+                                    className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">XS</label>
                             </div>
-                            <div class="size-selector">
-                                <input type="radio" name="size" id="size-sm" class="hidden" />
+                            <div className="size-selector">
+                                <input type="radio" name="size" id="size-sm" className="hidden" />
                                 <label for="size-sm"
-                                    class="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">S</label>
+                                    className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">S</label>
                             </div>
-                            <div class="size-selector">
-                                <input type="radio" name="size" id="size-m" class="hidden" />
+                            <div className="size-selector">
+                                <input type="radio" name="size" id="size-m" className="hidden" />
                                 <label for="size-m"
-                                    class="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">M</label>
+                                    className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">M</label>
                             </div>
-                            <div class="size-selector">
-                                <input type="radio" name="size" id="size-l" class="hidden" />
+                            <div className="size-selector">
+                                <input type="radio" name="size" id="size-l" className="hidden" />
                                 <label for="size-l"
-                                    class="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">L</label>
+                                    className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">L</label>
                             </div>
-                            <div class="size-selector">
-                                <input type="radio" name="size" id="size-xl" class="hidden" />
+                            <div className="size-selector">
+                                <input type="radio" name="size" id="size-xl" className="hidden" />
                                 <label for="size-xl"
-                                    class="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">XL</label>
+                                    className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600">XL</label>
                             </div>
                         </div>
                     </div>
 
-                    <div class="pt-4">
-                        <h3 class="text-xl text-gray-800 mb-3 uppercase font-medium">Color</h3>
-                        <div class="flex items-center gap-2">
-                            <div class="color-selector">
-                                <input type="radio" name="color" id="red" class="hidden" />
+                    <div className="pt-4">
+                        <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">Color</h3>
+                        <div className="flex items-center gap-2">
+                            <div className="color-selector">
+                                <input type="radio" name="color" id="red" className="hidden" />
                                 <label for="red"
-                                    class="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
+                                    className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
                                     style={{ backgroundColor: "#fefefe" }}></label>
                             </div>
-                            <div class="color-selector">
-                                <input type="radio" name="color" id="black" class="hidden" />
-                                <label for="black" class="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
+                            <div className="color-selector">
+                                <input type="radio" name="color" id="black" className="hidden" />
+                                <label for="black" className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
                                     style={{ backgroundColor: "red" }}></label>
                             </div>
-                            <div class="color-selector">
-                                <input type="radio" name="color" id="white" class="hidden" />
+                            <div className="color-selector">
+                                <input type="radio" name="color" id="white" className="hidden" />
                                 <label for="white"
-                                    class="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
+                                    className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
                                     style={{ backgroundColor: "#000" }}></label>
                             </div>
 
                         </div>
                     </div>
 
-                    <div class="mt-4">
-                        <h3 class="text-sm text-gray-800 uppercase mb-1">Quantity</h3>
-                        <div class="flex border border-gray-300 text-gray-600 divide-x divide-gray-300 w-max">
-                            <div class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">-</div>
-                            <input type='text' class="h-8 w-8 text-base flex items-center border-0 outline-none justify-center bg-white p-2" value='01'/>
-                            <div class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">+</div>
+                    <div className="mt-4">
+                        <h3 className="text-sm text-gray-800 uppercase mb-1">Quantity</h3>
+                        <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300 w-max">
+                            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">-</div>
+                            <input type='text' className="h-8 w-8 text-base flex items-center border-0 outline-none justify-center bg-white p-2" value='01'/>
+                            <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">+</div>
                         </div>
                     </div>
 
-                    <div class="mt-6 gap-3 border-b border-gray-200 pb-5 pt-5">
-                        <Link to="/checkout" class="rounded-none border border-primary text-white px-8 py-4 text-center font-medium uppercase flex items-center justify-center bg-gradient-to-l from-primary to-[#52a3eb] gap-2 hover:from-[#52a3eb] hover:to-primary transition">
+                    <div className="mt-6 gap-3 border-b border-gray-200 pb-5 pt-5">
+                        <Link to="/checkout" className="rounded-none border border-primary text-white px-8 py-4 text-center font-medium uppercase flex items-center justify-center bg-gradient-to-l from-primary to-[#52a3eb] gap-2 hover:from-[#52a3eb] hover:to-primary transition">
                             <RiShoppingBagFill/> Add to cart
                         </Link>
                     </div>
 
-                    <div class="flex gap-3 mt-4">
-                        <Link to="/" class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                    <div className="flex gap-3 mt-4">
+                        <Link to="/" className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <RiFacebookFill/>
                         </Link>
-                        <Link to="/" class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                        <Link to="/" className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <RiTwitterLine />
                         </Link>
-                        <Link to="/" class="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
+                        <Link to="/" className="text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center">
                             <RiInstagramLine/>
                         </Link>
                     </div>
